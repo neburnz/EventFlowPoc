2c41827aefe8054663a20d2e1b840f285bc501ba
Ruben Guzman <neburnz@hotmail.com>
2019-11-12 19:03:24 -0600

    Idempotencia de Comandos

--- eventflow.api/Commands/FinalizarProcesoCommand.cs
+++ eventflow.api/Commands/FinalizarProcesoCommand.cs
@@ -1,13 +1,18 @@
 using System;
+using System.Collections.Generic;
 using EventFlow.Aggregates.ExecutionResults;
 using EventFlow.Commands;
 
 namespace poc.eventflow
 {
-    public class FinalizarProcesoCommand : Command<ProcesoAggregate, ProcesoId, IExecutionResult>
+    public class FinalizarProcesoCommand : DistinctCommand<ProcesoAggregate, ProcesoId, IExecutionResult>
     {
         public FinalizarProcesoCommand(ProcesoId id) : base(id)
         {
+        }
+        protected override IEnumerable<byte[]> GetSourceIdComponents()
+        {
+            yield return System.Text.Encoding.UTF8.GetBytes(this.AggregateId.Value);
         }
     }
 }
--- eventflow.api/Commands/IniciarProcesoCommand.cs
+++ eventflow.api/Commands/IniciarProcesoCommand.cs
@@ -1,16 +1,21 @@
 using System;
+using System.Collections.Generic;
 using EventFlow.Aggregates.ExecutionResults;
 using EventFlow.Commands;
+using EventFlow.Core;
 
 namespace poc.eventflow
 {
-    public class IniciarProcesoCommand : Command<ProcesoAggregate, ProcesoId, IExecutionResult>
+    public class IniciarProcesoCommand : DistinctCommand<ProcesoAggregate, ProcesoId, IExecutionResult>
     {
         public IniciarProcesoCommand(ProcesoId id, DateTime fechaCorte) : base(id)
         {
             FechaCorte = fechaCorte;
         }
-
         public DateTime FechaCorte { get; }
+        protected override IEnumerable<byte[]> GetSourceIdComponents()
+        {
+            yield return BitConverter.GetBytes(FechaCorte.Ticks);
+        }
     }
 }
