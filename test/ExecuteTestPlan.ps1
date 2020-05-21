f17e8c7cbae1be26832420c422c1610ab3931d01
Ruben Guzman <neburnz@hotmail.com>
2020-05-21 16:07:53 -0500

    Ajuste script test

--- test/ExecuteTestPlan.ps1
+++ test/ExecuteTestPlan.ps1
@@ -1,12 +1,15 @@
-﻿$Env:Path += ";C:\Software\apache-jmeter-5.2\bin"
+﻿$jmeter_path = $args[0]
+$execution_path = $args[1]
+
+$Env:Path += ";$($jmeter_path)"
 
 $executionDate = Get-Date -Format "yyyy_MM_dd_HHmmss"
 
-$testPlanFile = "C:\Users\Admin\OneDrive - AN Global\Projects\EventFlow\poc\test\SIFACE.Monitor - Test Plan.jmx"
-$testResultsFile = "C:\Users\Admin\OneDrive - AN Global\Projects\EventFlow\poc\test\test_results_$($executionDate).jtl"
+$testPlanFile = "$($execution_path)\JMeter_Test_Plan.jmx"
+$testResultsFile = "$($execution_path)\test_results_$($executionDate).jtl"
 
 jmeter.bat -n -t "$($testPlanFile)" -l "$($testResultsFile)"
 
-$dashboardFolder = "C:\Users\Admin\OneDrive - AN Global\Projects\EventFlow\poc\test\dashboard_$($executionDate)"
+$dashboardFolder = "$($execution_path)\dashboard_$($executionDate)"
 
 jmeter.bat -g "$($testResultsFile)" -o "$($dashboardFolder)"
