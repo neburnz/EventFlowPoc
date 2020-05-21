ad60dd539b24d1f6fa597d84935f224811c6ab9b
Ruben Guzman <neburnz@hotmail.com>
2020-05-21 13:22:56 -0500

    Agrega archivos de prueba con JMeter

---
+++ test/ExecuteTestPlan.ps1
@@ -0,0 +1,12 @@
+﻿$Env:Path += ";C:\Software\apache-jmeter-5.2\bin"
+
+$executionDate = Get-Date -Format "yyyy_MM_dd_HHmmss"
+
+$testPlanFile = "C:\Users\Admin\OneDrive - AN Global\Projects\EventFlow\poc\test\SIFACE.Monitor - Test Plan.jmx"
+$testResultsFile = "C:\Users\Admin\OneDrive - AN Global\Projects\EventFlow\poc\test\test_results_$($executionDate).jtl"
+
+jmeter.bat -n -t "$($testPlanFile)" -l "$($testResultsFile)"
+
+$dashboardFolder = "C:\Users\Admin\OneDrive - AN Global\Projects\EventFlow\poc\test\dashboard_$($executionDate)"
+
+jmeter.bat -g "$($testResultsFile)" -o "$($dashboardFolder)"
